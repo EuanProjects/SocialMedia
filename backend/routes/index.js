66a424262fe9c2256e692d4a5c9d406fe94b46ca
ejcanoy <euan.james.canoy@gmail.com>
2024-09-17 10:56:02 -0700

    Added testing for frontend and backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('home');
+  res.send({message: 'home'});
 });
 
 module.exports = router;
