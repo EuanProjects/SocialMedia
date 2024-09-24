5c06b167ead1bfae01b4af59cc63bed3ad56dcf7
ejcanoy <euan.james.canoy@gmail.com>
2024-09-24 16:23:58 -0700

    finished all user tests

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,5 +4,7 @@ const userController = require("../controller/user")
 
 router.post('/', userController.postUser);
 router.get('/:userId', userController.getUser)
+router.put('/:userId', userController.putUser)
+router.delete('/:userId', userController.deleteUser)
 
 module.exports = router;
