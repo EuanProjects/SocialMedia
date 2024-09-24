d30d38ebddce9dd4145f6bd78b1dcaca4defee0d
ejcanoy <euan.james.canoy@gmail.com>
2024-09-24 15:44:36 -0700

    created user create and get routes

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -54,4 +54,31 @@ exports.deleteUser = asyncHandler(async (req, res) => {
     })
 
     res.status(200).json({})
+})const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+const asyncHandler = require("express-async-handler")
+
+exports.postUser = asyncHandler(async (req, res) => {
+    const newUser = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    });
+    console.log(newUser);
+    res.status(200).json({ id: newUser.id });
+});
+
+exports.getUser = asyncHandler(async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.params.userId
+        }
+    })
+
+    res.status(200).json({
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+    })
 })
