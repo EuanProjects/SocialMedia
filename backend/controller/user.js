f8f91f1f2756e5954b00b65258ad4c1622c552d2
ejcanoy <euan.james.canoy@gmail.com>
2024-09-24 15:44:36 -0700

    created user create and get routes

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -9,6 +9,7 @@ exports.postUser = asyncHandler(async (req, res) => {
             password: req.body.password
         }
     });
+    console.log(newUser);
     res.status(200).json({ id: newUser.id });
 });
 
@@ -24,28 +25,4 @@ exports.getUser = asyncHandler(async (req, res) => {
         username: user.username,
         picture: user.picture
     })
-})
-
-exports.putUser = asyncHandler(async (req, res) => {
-    const updatedUser = await prisma.user.update({
-        where: {
-            id: req.params.userId
-        },
-        data: req.body
-    })
-    res.status(200).json({
-        id: updatedUser.id,
-        username: updatedUser.username,
-        picture: updatedUser.picture
-    })
-})
-
-exports.deleteUser = asyncHandler(async (req, res) => {
-    const deletedUser = await prisma.user.delete({
-        where: {
-            id: req.params.userId
-        }
-    })
-
-    res.status(200).json({})
 })
