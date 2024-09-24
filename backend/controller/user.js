82a974c7d159727ad47c6fb39cdcff17a69b08a2
ejcanoy <euan.james.canoy@gmail.com>
2024-09-24 16:23:58 -0700

    finished all user tests

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -9,7 +9,6 @@ exports.postUser = asyncHandler(async (req, res) => {
             password: req.body.password
         }
     });
-    console.log(newUser);
     res.status(200).json({ id: newUser.id });
 });
 
@@ -25,4 +24,28 @@ exports.getUser = asyncHandler(async (req, res) => {
         username: user.username,
         picture: user.picture
     })
+})
+
+exports.putUser = asyncHandler(async (req, res) => {
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: req.params.userId
+        },
+        data: req.body
+    })
+    res.status(200).json({
+        id: updatedUser.id,
+        username: updatedUser.username,
+        picture: updatedUser.picture
+    })
+})
+
+exports.deleteUser = asyncHandler(async (req, res) => {
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: req.params.userId
+        }
+    })
+
+    res.status(200).json({})
 })
