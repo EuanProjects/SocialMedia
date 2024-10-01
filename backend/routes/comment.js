398da24c22fa484294a471167cd8570453b305a0
ejcanoy <euan.james.canoy@gmail.com>
2024-10-01 10:29:38 -0700

    finished backend tests

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,5 +3,8 @@ const router = express();
 const commentController = require('../controller/comment');
 
 router.post('/', commentController.postComment)
+router.get('/:commentId', commentController.getComment)
+router.put('/:commentId', commentController.putComment)
+router.delete('/:commentId', commentController.deleteComment)
 
 module.exports = router
