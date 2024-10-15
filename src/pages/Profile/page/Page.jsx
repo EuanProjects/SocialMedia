fec04527ec3776a5dfb83c86b64d4df197569ec7
ejcanoy <euan.james.canoy@gmail.com>
2024-10-15 14:57:23 -0700

    fixed gap

--- src/pages/Profile/page/Page.jsx
+++ src/pages/Profile/page/Page.jsx
@@ -21,7 +21,7 @@ export default function Page() {
                         </div>
 
                     </div>
-                    <div className="col-span-2 p-2">
+                    <div className="col-span-2 p-2 flex flex-col gap-2">
                         {
                             
                             posts && posts.posts
