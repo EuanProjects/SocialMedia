f0a8ae7261878a5909cb1fda30ae1763c5f2c055
ejcanoy <euan.james.canoy@gmail.com>
2024-10-15 23:08:12 -0700

    Added navigate to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/login')
+    }
+  }, [location, navigate])
 
   return (
     <>
